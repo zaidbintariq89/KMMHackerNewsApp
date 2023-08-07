04574d87e74ce43d2fc3587211b0cce89dd50379
Zaid Bin Tariq <zaid@innowi.com>
2023-08-07 16:25:08 +0500

    Main composable UI with data

--- androidApp/src/main/java/com/example/kmmtestapp/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/kmmtestapp/android/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.kmmtestapp.android
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
@@ -15,6 +16,10 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,8 +30,15 @@ import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import com.example.kmmtestapp.Greeting
+import com.example.kmmtestapp.kmm.shared.SpaceXSDK
+import com.example.kmmtestapp.kmm.shared.cache.DatabaseDriverFactory
+import com.example.kmmtestapp.kmm.shared.entity.RocketLaunch
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private val spaceXSdk: SpaceXSDK = SpaceXSDK(DatabaseDriverFactory(this))
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -35,7 +47,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    GreetingView(Greeting().greet(), spaceXSdk)
                 }
             }
         }
@@ -43,12 +55,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingView(text: String) {
-    val languages = listOf(
-        "C++", "C", "C#", "Java", "Kotlin", "Dart", "Python", "Javascript", "SpringBoot",
-        "XML", "Dart", "Node JS", "Typescript", "Dot Net", "GoLang", "MongoDb",
-    )
-
+fun GreetingView(text: String, spaceXSdk: SpaceXSDK? = null) {
+    val rocketState = remember {
+        mutableStateOf(emptyList<RocketLaunch>())
+    }
+    val mainScope = rememberCoroutineScope()
     Column(
         // inside this column we are specifying modifier
         // to specify max width and max height
@@ -62,17 +73,40 @@ fun GreetingView(text: String) {
             text = text,
             modifier = Modifier.padding(10.dp),
             style = TextStyle(
-                color = Color.White,
+                color = Color.Black,
                 fontSize = TextUnit(value = 20.0F, type = TextUnitType.Sp)
             ),
             fontWeight = FontWeight.Bold
         )
 
-        LazyColumn {
-            items(languages) { language ->
-                Text(language , modifier = Modifier.padding(15.dp))
-                Divider()
+        // API call
+        LaunchedEffect(Unit) {
+            mainScope.launch {
+                kotlin.runCatching {
+                    spaceXSdk?.getLaunches(forceReload = false)
+                }.onSuccess {
+                    Log.d("MainActivity", "Data received = ${it?.toString()}")
+                    it?.let { rockets ->
+                        rocketState.value = rockets.sortedByDescending { rocket-> rocket.launchYear }
+                    }
+                }.onFailure {
+                    Log.d("MainActivity", "Data fetch error")
+                }
             }
+        }
+
+        // bind data
+        ItemsListView(list = rocketState.value)
+    }
+}
+
+@Composable
+fun ItemsListView(list: List<RocketLaunch>) {
+    LazyColumn {
+        items(list) { rocket ->
+            val details = "${rocket.missionName} | ${rocket.launchYear} | ${rocket.launchSuccess ?: false}"
+            Text(details , modifier = Modifier.padding(15.dp))
+            Divider()
         }
     }
 }
