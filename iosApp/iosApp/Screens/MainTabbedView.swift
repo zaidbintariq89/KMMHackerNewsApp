3e3a23f332cdea517ecdc0af899051641d387b88
Kaleem Asad <kaleem.asad@mobilelive.ca>
2023-09-24 02:18:51 +0500

    Top Header View added

--- iosApp/iosApp/Screens/MainTabbedView.swift
+++ iosApp/iosApp/Screens/MainTabbedView.swift
@@ -1,65 +1,118 @@
 import SwiftUI
 import shared
 
+// MainTabbedView is the main SwiftUI view for your tabbed interface.
 struct MainTabbedView: View {
     
+    // State object to manage reactive properties
     @StateObject private var currentReactives = SetReactiveMethods()
+    
+    // State variable to keep track of the selected tab
     @State var tabSelected = 0
     
+    // Initialize the view
     init(){
+        // Set the appearance of the UITabBar
         UITabBar.appearance().backgroundColor = .white
     }
     
+    // Create an instance of NetworkRepo for data fetching
     let sdk = NetworkRepo(databaseDriverFactory: DatabaseDriverFactory())
     
-    var body: some View {
-        ZStack {
-            TabView(selection: $tabSelected) {
-                
-                ServicesView()
-                    .tabItem {
-                        Image(systemName: "iphone")
-                        Text("Services") // Add text to the tab
-                    }
-                    .tag(0)
-                
-                BillingView(viewModel: .init(sdk: sdk))
-                    .tabItem {
-                        Image(systemName: "dollarsign.square")
-                        Text("Billing") // Add text to the tab
-                    }
-                    .tag(1)
-                
-                MessagesView()
-                    .tabItem {
-                        Image(systemName: "message.badge.filled.fill")
-                        Text("Messages") // Add text to the tab
-                    }
-                    .tag(2)
-                
-                SupportView()
-                    .tabItem {
-                        Image(systemName: "message")
-                        Text("Support") // Add text to the tab
-                    }
-                    .tag(3)
-                
-                MoreView()
-                    .tabItem {
-                        Image(systemName: "ellipsis")
-                        Text("More") // Add text to the tab
-                    }
-                    .tag(4)
+    // Define the top header view
+    fileprivate func topHeaderView() -> some View {
+        return ZStack {
+            Color.red
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                HStack {
+                    Spacer(minLength: 20)
+                    
+                    // Welcome text with styling
+                    Text("Welcome, User!")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    
+                    Spacer()
+                    
+                    // Profile image icon
+                    Image(systemName: "person.circle")
+                        .resizable()
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .frame(width: 20, height: 20) // Fixed width and height
+                        .padding(20)
+                }
                 
             }
-            .accentColor(.pink)
+            .frame(height: 90)
+        }
+        .frame(height: 30)
+    }
+    
+    var body: some View {
+        VStack {
             
-            FullScreenNavigationView(isShowing: $currentReactives.presentFullScreenView, content: AnyView(NavigationView { currentReactives.viewToShow.view }))
+            // Include the top header view in the main layout
+            topHeaderView()
             
+            ZStack {
+                TabView(selection: $tabSelected) {
+                    
+                    // Tab 1: ServicesView
+                    ServicesView()
+                        .tabItem {
+                            Image(systemName: "iphone")
+                            Text("Services") // Add text to the tab
+                        }
+                        .tag(0)
+                    
+                    // Tab 2: BillingView
+                    BillingView(viewModel: .init(sdk: sdk))
+                        .tabItem {
+                            Image(systemName: "dollarsign.square")
+                            Text("Billing") // Add text to the tab
+                        }
+                        .tag(1)
+                    
+                    // Tab 3: MessagesView
+                    MessagesView()
+                        .tabItem {
+                            Image(systemName: "message.badge.filled.fill")
+                            Text("Messages") // Add text to the tab
+                        }
+                        .tag(2)
+                    
+                    // Tab 4: SupportView
+                    SupportView()
+                        .tabItem {
+                            Image(systemName: "message")
+                            Text("Support") // Add text to the tab
+                        }
+                        .tag(3)
+                    
+                    // Tab 5: MoreView
+                    MoreView()
+                        .tabItem {
+                            Image(systemName: "ellipsis")
+                            Text("More") // Add text to the tab
+                        }
+                        .tag(4)
+                    
+                }
+                .accentColor(.pink)
+                
+                // Full-screen navigation view
+                FullScreenNavigationView(isShowing: $currentReactives.presentFullScreenView, content: AnyView(NavigationView { currentReactives.viewToShow.view }))
+                
+            }
         }
     }
 }
 
+// Preview provider for MainTabbedView
 struct MainTabbedView_Previews: PreviewProvider {
     static var previews: some View {
         MainTabbedView()
