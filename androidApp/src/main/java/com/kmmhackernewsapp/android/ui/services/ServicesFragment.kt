f36f37219cd78aed69ba1d383743852415f4714b
zaidbintariq <zaid.tariq@mobilelive.ca>
2023-09-24 00:29:05 +0500

    services ui in progress

--- androidApp/src/main/java/com/kmmhackernewsapp/android/ui/services/ServicesFragment.kt
+++ androidApp/src/main/java/com/kmmhackernewsapp/android/ui/services/ServicesFragment.kt
@@ -5,11 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.kmmhackernewsapp.android.R
 import com.kmmhackernewsapp.android.databinding.ServicesFragmentBinding
 
 class ServicesFragment : Fragment() {
     private lateinit var binding: ServicesFragmentBinding
+    private var viewModel: ServicesViewModel? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,5 +25,6 @@ class ServicesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         activity?.setTitle(R.string.services_lbl)
+        viewModel = ViewModelProvider(this)[ServicesViewModel::class.java]
     }
 }
