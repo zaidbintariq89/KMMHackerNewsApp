4749acc822112383c960568a06eeb68c5a5e1f7c
Kaleem Asad <kaleem.asad@mobilelive.ca>
2023-09-24 02:22:27 +0500

    Intentation

--- iosApp/iosApp/Screens/Views/UsageView.swift
+++ iosApp/iosApp/Screens/Views/UsageView.swift
@@ -7,16 +7,22 @@
 //
 
 import SwiftUI
+import shared
 
+/// `UsageView` is a SwiftUI view that displays a carousel of `BannerView` items along with a `TitleDescriptionView`.
 struct UsageView: View {
     var body: some View {
-        VStack{
+        VStack {
+            // Carousel of BannerView items
             CarouselView()
                 .frame(maxWidth: .infinity)
                 .edgesIgnoringSafeArea(.all)
                 .background(Color.white)
                 .frame(height: 170)
+            
+            // Title and description view
             TitleDescriptionView()
+            
             Spacer()
         }
     }
@@ -28,6 +34,7 @@ struct UsageView_Previews: PreviewProvider {
     }
 }
 
+/// `BannerView` is a SwiftUI view that displays a banner with an image, title, description, and a link.
 struct BannerView: View {
     let imageName: String
     let title: String
@@ -36,6 +43,7 @@ struct BannerView: View {
     
     var body: some View {
         HStack {
+            // Banner image
             Image(imageName)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
@@ -43,21 +51,21 @@ struct BannerView: View {
                 .cornerRadius(20)
             
             VStack(alignment: .leading) {
+                // Banner title
                 Text(title)
                     .font(.body)
                     .padding(.bottom, 2)
                     .foregroundColor(.secondary)
                 
-                
-                Text(description) // Display the description
+                // Banner description with multiline support
+                Text(description)
                     .font(.footnote)
-                    .multilineTextAlignment(.leading) // Align text to the left
+                    .multilineTextAlignment(.leading)
                     .lineLimit(2)
-                //                    .padding(.bottom, 1)
                 
+                // Banner link
                 Link("Open Link", destination: url)
                     .foregroundColor(.blue)
-                
             }
             .padding()
         }
@@ -68,6 +76,7 @@ struct BannerView: View {
     }
 }
 
+/// `CarouselView` is a SwiftUI view that displays a carousel of `BannerView` items.
 struct CarouselView: View {
     var body: some View {
         TabView {
@@ -81,51 +90,53 @@ struct CarouselView: View {
     }
 }
 
-
-
+/// `TitleDescriptionView` is a SwiftUI view that displays a title, description, and a progress bar.
 struct TitleDescriptionView: View {
     @State private var progressValue: Double = 0.6 // Adjust this value as needed
     
     var body: some View {
         VStack {
+            // Title text with customized font size and weight
             Text("2.31 GB")
-                .font(.system(size: 52, weight: .bold)) // Customize the font size (32) and weight (bold)
+                .font(.system(size: 52, weight: .bold))
                 .foregroundColor(AppColors.appGreen)
                 .padding(.bottom, 10)
             
+            // Description text
             Text("Remaining Data")
                 .font(.title2)
                 .foregroundColor(AppColors.appGreen)
                 .multilineTextAlignment(.center)
                 .padding(.horizontal, 20)
             
+            // Progress bar with padding and shadow
             ProgressBar(progress: progressValue)
-                .padding(.horizontal, 20) // 20 units of spacing from leading and trailing edges
-                .padding(.vertical, 10) // Add vertical padding inside the progress bar
-                .shadow(color: Color.black.opacity(0.3), radius: 5, x: 0, y: 5) // Add shadow to the progress bar
+                .padding(.horizontal, 20)
+                .padding(.vertical, 10)
+                .shadow(color: Color.black.opacity(0.3), radius: 5, x: 0, y: 5)
         }
         .frame(maxWidth: .infinity)
         .edgesIgnoringSafeArea(.all)
-        .background(Color.white) // Add white background color to the entire view
-        
+        .background(Color.white)
     }
 }
 
+/// `ProgressBar` is a SwiftUI view that displays a progress bar with rounded corners.
 struct ProgressBar: View {
     let progress: Double
     
     var body: some View {
         GeometryReader { geometry in
             ZStack(alignment: .leading) {
-                RoundedRectangle(cornerRadius: 10) // Rounded corners with a radius of 10
+                RoundedRectangle(cornerRadius: 10)
                     .foregroundColor(.white)
-                    .frame(width: geometry.size.width, height: 20) // Use the full width of the container
+                    .frame(width: geometry.size.width, height: 20)
                 
-                RoundedRectangle(cornerRadius: 10) // Rounded corners with a radius of 10
+                RoundedRectangle(cornerRadius: 10)
                     .foregroundColor(AppColors.appGreen)
                     .frame(width: CGFloat(self.progress) * (geometry.size.width), height: 20)
             }
         }
-        .frame(height: 20) // Set the height to 20
+        .frame(height: 20)
     }
 }
