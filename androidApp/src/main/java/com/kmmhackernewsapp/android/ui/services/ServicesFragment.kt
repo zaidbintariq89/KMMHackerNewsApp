c983d82be1a07239af0a97882d88b456a6352bf4
zaidbintariq <zaid.tariq@mobilelive.ca>
2023-09-24 01:03:31 +0500

    services ui in progress

--- androidApp/src/main/java/com/kmmhackernewsapp/android/ui/services/ServicesFragment.kt
+++ androidApp/src/main/java/com/kmmhackernewsapp/android/ui/services/ServicesFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.tabs.TabLayoutMediator
 import com.kmmhackernewsapp.android.R
 import com.kmmhackernewsapp.android.databinding.ServicesFragmentBinding
 
@@ -26,5 +27,23 @@ class ServicesFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         activity?.setTitle(R.string.services_lbl)
         viewModel = ViewModelProvider(this)[ServicesViewModel::class.java]
+        initPagers()
+    }
+
+    private fun initPagers() {
+        activity?.let {
+            val adapter = ServicesPagerAdapter(it)
+            binding.servicesPager.adapter = adapter
+
+            // Attach the TabLayout to the ViewPager2
+            TabLayoutMediator(binding.servicesTab, binding.servicesPager) { tab, position ->
+                tab.text = when(position) {
+                    0 -> "Usage"
+                    1 -> "Equipment"
+                    2 -> "Package"
+                    else -> ""
+                }
+            }.attach()
+        }
     }
 }
