3c35c48712b72ba42ec21b3a4777ff72cc76a159
Kaleem Asad <kaleem.asad@mobilelive.ca>
2023-09-24 04:18:12 +0500

    Added Support screen header view Ask Anna

--- iosApp/iosApp/Screens/SupportView.swift
+++ iosApp/iosApp/Screens/SupportView.swift
@@ -8,41 +8,46 @@ import SwiftUI
 
 struct SupportView: View {
     var body: some View {
-        NavigationView {
-            List {
-                Section() {
-                    Image("header")
-                        .resizable()
-                              .frame(height: 150)
-                              .listRowInsets(EdgeInsets())
-                }
-                ForEach((0..<2), id: \.self) { index in
-                    Section {
-                        NavigationLink(destination: Text("aaa")) {
-                            Label("Billing & Accounts", systemImage: "capsule")
-                        }
-                        NavigationLink(destination: Text("aaa")) {
-                            Label("Mobile", systemImage: "paintpalette")
-                        }
-                        NavigationLink(destination: Text("aaa")) {
-                            Label("Home phone", systemImage: "paintpalette")
-                        }
-                        NavigationLink(destination: Text("aaa")) {
-                            Label("Internet", systemImage: "paintpalette")
-                        }
-                        NavigationLink(destination: Text("aaa")) {
-                            Label("TV & Streaming", systemImage: "paintpalette")
-                        }
-                        NavigationLink(destination: Text("aaa")) {
-                            Label("Home Monitoring", systemImage: "paintpalette")
-                        }
-                    }
-                }
-            }
-            .navigationBarTitle("Support")
-            .navigationBarTitleDisplayMode(.inline)
+        VStack {
+            SupportHeaderView()
+            Spacer()
         }
-        .accentColor(.accentColor)
+        
+        //        NavigationView {
+        //            List {
+        //                Section() {
+        //                    Image("header")
+        //                        .resizable()
+        //                              .frame(height: 150)
+        //                              .listRowInsets(EdgeInsets())
+        //                }
+        //                ForEach((0..<2), id: \.self) { index in
+        //                    Section {
+        //                        NavigationLink(destination: Text("aaa")) {
+        //                            Label("Billing & Accounts", systemImage: "capsule")
+        //                        }
+        //                        NavigationLink(destination: Text("aaa")) {
+        //                            Label("Mobile", systemImage: "paintpalette")
+        //                        }
+        //                        NavigationLink(destination: Text("aaa")) {
+        //                            Label("Home phone", systemImage: "paintpalette")
+        //                        }
+        //                        NavigationLink(destination: Text("aaa")) {
+        //                            Label("Internet", systemImage: "paintpalette")
+        //                        }
+        //                        NavigationLink(destination: Text("aaa")) {
+        //                            Label("TV & Streaming", systemImage: "paintpalette")
+        //                        }
+        //                        NavigationLink(destination: Text("aaa")) {
+        //                            Label("Home Monitoring", systemImage: "paintpalette")
+        //                        }
+        //                    }
+        //                }
+        //            }
+        //            .navigationBarTitle("Support")
+        //            .navigationBarTitleDisplayMode(.inline)
+        //        }
+        //        .accentColor(.accentColor)
     }
 }
 
---
+++ iosApp/iosApp/Views/SupportHeaderView.swift
@@ -0,0 +1,52 @@
+//
+//  SupportHeaderView.swift
+//  iosApp
+//
+//  Created by Kaleem Asad on 24/09/2023.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+/// `SupportHeaderView` is a SwiftUI view that displays a header for a support screen.
+struct SupportHeaderView: View {
+    var body: some View {
+        ZStack {
+            Color.red // Set the background color
+            
+            HStack {
+                VStack(alignment: .leading, spacing: 8) { // Align text to the left
+                    Text("Ask Anna")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    
+                    Text("Need Help?")
+                        .font(.title)
+                        .foregroundColor(.white)
+                    
+                    Button(action: {
+                        // Add your action here for starting a chat
+                    }) {
+                        Text("Start a chat")
+                            .font(.headline)
+                            .fontWeight(.medium)
+                            .padding(10)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .foregroundColor(.red)
+                    }
+                }
+                .padding(.leading, 20) // Adjust the trailing padding as needed
+                Spacer()
+                Image(systemName: "message.badge")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .foregroundColor(.white)
+                    .padding(.trailing, 30)
+            }
+        }
+        .frame(height: 170) // Adjust the height as needed
+        Spacer()
+    }
+}
