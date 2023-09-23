d602d6cb7fa594ff3ee59e063eca87aab11fa1ed
Kaleem Asad <kaleemasadmughal@gmail.com>
2023-09-24 01:38:26 +0500

    Progress View added

--- iosApp/iosApp/Screens/Views/UsageView.swift
+++ iosApp/iosApp/Screens/Views/UsageView.swift
@@ -16,6 +16,7 @@ struct UsageView: View {
                 .edgesIgnoringSafeArea(.all)
                 .background(Color.white)
                 .frame(height: 170)
+            TitleDescriptionView()
             Spacer()
         }
     }
@@ -46,13 +47,13 @@ struct BannerView: View {
                     .font(.body)
                     .padding(.bottom, 2)
                     .foregroundColor(.secondary)
-
+                
                 
                 Text(description) // Display the description
                     .font(.footnote)
                     .multilineTextAlignment(.leading) // Align text to the left
                     .lineLimit(2)
-//                    .padding(.bottom, 1)
+                //                    .padding(.bottom, 1)
                 
                 Link("Open Link", destination: url)
                     .foregroundColor(.blue)
@@ -77,5 +78,54 @@ struct CarouselView: View {
         }
         .tabViewStyle(PageTabViewStyle())
         .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+    }
+}
+
+
+
+struct TitleDescriptionView: View {
+    @State private var progressValue: Double = 0.6 // Adjust this value as needed
+    
+    var body: some View {
+        VStack {
+            Text("2.31 GB")
+                .font(.system(size: 52, weight: .bold)) // Customize the font size (32) and weight (bold)
+                .foregroundColor(AppColors.appGreen)
+                .padding(.bottom, 10)
+            
+            Text("Remaining Data")
+                .font(.title2)
+                .foregroundColor(AppColors.appGreen)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 20)
+            
+            ProgressBar(progress: progressValue)
+                .padding(.horizontal, 20) // 20 units of spacing from leading and trailing edges
+                .padding(.vertical, 10) // Add vertical padding inside the progress bar
+                .shadow(color: Color.black.opacity(0.3), radius: 5, x: 0, y: 5) // Add shadow to the progress bar
+        }
+        .frame(maxWidth: .infinity)
+        .edgesIgnoringSafeArea(.all)
+        .background(Color.white) // Add white background color to the entire view
+        
+    }
+}
+
+struct ProgressBar: View {
+    let progress: Double
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                RoundedRectangle(cornerRadius: 10) // Rounded corners with a radius of 10
+                    .foregroundColor(.white)
+                    .frame(width: geometry.size.width, height: 20) // Use the full width of the container
+                
+                RoundedRectangle(cornerRadius: 10) // Rounded corners with a radius of 10
+                    .foregroundColor(AppColors.appGreen)
+                    .frame(width: CGFloat(self.progress) * (geometry.size.width), height: 20)
+            }
+        }
+        .frame(height: 20) // Set the height to 20
     }
 }
---
+++ iosApp/iosApp/Utilities/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  iosApp
+//
+//  Created by Kaleem Asad on 24/09/2023.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+
+
+struct AppColors {
+    static let primaryColor = Color.blue
+    static let secondaryColor = Color.green
+    static let textColor = Color.black
+    static let backgroundColor = Color.white
+    // Add more color constants as needed
+    
+    // Custom Colors
+    static let appGreen = Color("AppGreen")
+
+}
